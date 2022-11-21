3cd3837209c68901f14e490e2b137abb2723418c
Enzo Vieira <enzovieira16@hotmail.com>
2022-11-21 22:08:21 +0000

    Terminado o desafio

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,14 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    listLength(list, 0)
+  end
+
+  defp listLength([], acc) do
+    acc
+  end
+
+  defp listLength([_ | t], acc) do
+    acc = acc + 1
+    listLength(t, acc)
   end
 end
